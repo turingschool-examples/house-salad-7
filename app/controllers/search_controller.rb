769473049c20e749c2a56d14dc510c4a5aa615d6
Michael Dao <mike@turing.edu>
2023-02-03 09:36:05 -0700

    refactoring in progress

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,17 @@
 class SearchController < ApplicationController
   def index
-    state = params[:state]
+    @facade = SearchFacade.new(params[:state])
+    # state = params[:state]
 
-    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
-    end
+    # conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+    #   faraday.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
+    # end
 
-    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+    # response = conn.get("/congress/v1/members/house/#{state}/current.json")
 
-    json = JSON.parse(response.body, symbolize_names: true)
-    @members = json[:results]
+    # json = JSON.parse(response.body, symbolize_names: true)
+    # @members = json[:results].map do |member_data|
+    #   Member.new(member_data)
+    # end
   end
 end
---
+++ app/facades/search_facade.rb
@@ -0,0 +1,18 @@
+class SearchFacade
+  def initialize(state)
+    @state = state
+  end
+
+  def members
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
+    end
+
+    response = conn.get("/congress/v1/members/house/#{@state}/current.json")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @members = json[:results].map do |member_data|
+      Member.new(member_data)
+    end
+  end
+end
