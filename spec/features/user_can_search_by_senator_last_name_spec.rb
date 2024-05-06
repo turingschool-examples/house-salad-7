d2c7322796f1a743f608b50fba1ac38a0a14c2e8
Jamison Ordway <jamisonordway@gmail.com>
2024-05-06 15:05:25 -0600

    Update for use with Congress.gov API

--- spec/features/user_can_search_by_senator_last_name_spec.rb
+++ spec/features/user_can_search_by_senator_last_name_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'Govt Search' do
+RSpec.describe 'Senator Search' do
   describe 'happy path' do
-    it 'allows user to search for govt members' do
+    it 'allows user to search for Senators by last name' do
       visit root_path
 
       fill_in :search, with: 'Sanders'
       click_button 'Search'
 
       expect(page.status_code).to eq 200
-      expect(page).to have_content("Senator Bernard Sanders was found!")
-      expect(page).to have_content("SenSanders")
+      expect(page).to have_content('Sanders, Bernard was found!')
+      expect(page.find('.senator-image')['src']).to have_content('https://www.congress.gov/img/member/s000033_200.jpg')
     end
 
     it 'allows user to search for another govt member' do
@@ -20,8 +20,8 @@ RSpec.describe 'Govt Search' do
       click_button 'Search'
 
       expect(page.status_code).to eq 200
-      expect(page).to have_content("Senator Cory Booker was found!")
-      expect(page).to have_content("SenBooker")
+      expect(page).to have_content("Booker, Cory A. was found!")
+      expect(page.find('.senator-image')['src']).to have_content('https://www.congress.gov/img/member/b001288_200.jpg')
     end
   end
 end
